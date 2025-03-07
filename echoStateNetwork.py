0f739ec084145bf326d12b18edcc6f1c1d25d8cd
Leon Scharwächter <56418155+lscharwaechter@users.noreply.github.com>
2025-03-07 17:18:43 +0100

    Update echoStateNetwork.py

--- echoStateNetwork.py
+++ echoStateNetwork.py
@@ -22,99 +22,80 @@ class ESN():
         self.n_output = n_output
         
     def resetStates(self, steps):
-        # Initialize net input and activation of
-        # the reservoir and output layer with 0
+        # Initialize net input and activation of the reservoir and output layer with 0
         self.net_h = np.zeros((self.n_reservoir,steps))
         self.act_h = np.zeros((self.n_reservoir,steps))   
         self.net_hk = np.zeros((self.n_output,steps))
         self.act_hk = np.zeros((self.n_output,steps))
         
-        
     def train(self, x, steps):
         self.resetStates(x.shape[1])
-        
-        prevt = 0
-        for t in range(steps):           
-            # Calculate activation of the reservoir:
-            # First integrate feed-forward input
-            for i in range(self.n_input):
-                for j in range(self.n_reservoir):
-                    self.net_h[j,t] += x[i,t]*self.Win[i,j]  
-                    
-            # Then integrate recurrent input with previous timestep
-            for i in range(self.n_reservoir):
-                for j in range(self.n_reservoir):
-                    self.net_h[j,t] += self.act_h[j,prevt]*self.Wr[i,j]
-            self.act_h = np.tanh(self.net_h) 
+    
+        prevt = 0  # Previous time step
+        for t in range(steps):
+            # Calculate activation of the reservoir
+            self.net_h[:, t] = np.dot(self.Win.T, x[:, t])
+    
+            # Integrate recurrent input with previous timestep
+            self.net_h[:, t] += np.dot(self.Wr, self.act_h[:, prevt])
             
+            # Apply activation function
+            self.act_h[:, t] = +np.tanh(0.9 * self.act_h[:, prevt] + self.net_h[:, t])
+            self.act_h[:, t] = np.clip(self.act_h[:, t], -0.8, 0.8)
+    
             # Calculate activation of the output layer
-            for i in range(self.n_reservoir):
-                for j in range(self.n_output):
-                    self.net_hk[j,t] += self.act_h[i,t]*self.Wout[i,j]
-            # No tanh nonlinearity
-            self.act_hk = self.net_hk
-              
-            # Set previous timestep for next iteration
-            if t > 0:
-                prevt = t
-                
-        T = np.transpose(self.act_hk)
-        
-        # Teacher Forcing:
-        # Replace the predicted output with 
-        # the ground truth target (sequence)
-        T = np.transpose(x)[:steps]
-        
-        # Determine M and new Wout
-        M = np.transpose(self.act_h[:,:steps])
-        Wout_new = np.dot(np.linalg.pinv(M),T)
-        
-        plt.plot(T[:,0])
+            self.net_hk[:, t] = np.dot(self.Wout.T, self.act_h[:, t])
+            self.act_hk[:, t] = self.net_hk[:, t]  # No tanh nonlinearity
+    
+            prevt = t  # Update previous timestep
+    
+        # Teacher Forcing
+        T = np.transpose(x[:, :steps])
+    
+        # Compute output weights (Wout_new)
+        M = np.transpose(self.act_h[:, :steps])
+        Wout_new = np.dot(np.linalg.pinv(M), T)  # Use pseudoinverse
+            
+        ### PLOT Original Signal ###
+        plt.plot(T[:-2, 0])
         
         return T, Wout_new
-    
+
+
     def predict(self, x, T, Wout_new, trainingSteps, testingSteps):
-        
-        # Start the Closed Loop with the last output of
-        # the training procedure
-        cl_output = T[-1,:]
-        
+        cl_output = x[:, trainingSteps - 1]  # Begin with last training value
+    
         prevt = trainingSteps
         for t in range(testingSteps):
-            
-            t = t+trainingSteps
-            
-            # Calculate activation of the reservoir,
-            # this time using Wfb
-            for i in range(self.n_input):
-                for j in range(self.n_reservoir):
-                    self.net_h[j,t] += cl_output*self.Wfb[i,j]  
-            for i in range(self.n_reservoir):
-                for j in range(self.n_reservoir):
-                    self.net_h[j,t] += self.act_h[j,prevt]*self.Wr[i,j]
-            self.act_h = np.tanh(self.net_h) 
-            
-            # Calculate activation of the output layer,
-            # this time using Wout_new
-            for i in range(self.n_reservoir):
-                for j in range(self.n_output):
-                    self.net_hk[j,t] += self.act_h[i,t]*Wout_new[i,j]
-            # No tanh nonlinearity
-            self.act_hk = self.net_hk
-            
-            cl_output = self.act_hk[:,t]
-              
-            # Set previous timestep for next iteration
-            prevt = t
-        
-        # Calculate Normalized RMSE    
-        NRMSE = np.sqrt(np.mean((self.act_hk[:,trainingSteps:]-x[:,trainingSteps:])**2)/np.var(x[:,trainingSteps:]))      
-        
-        ### PLOT ###
-        x_values = np.arange(trainingSteps,trainingSteps+testingSteps)
-        x_values = np.transpose(x_values)
-        plt.plot(x_values,self.act_hk[0,trainingSteps:])
-        #plt.vlines(trainingSteps, ymin = -1, ymax = 1, colors='r')
+            t = t + trainingSteps  # Update time step
+    
+            # Reset states before accumulation
+            self.net_h[:, t] = 0  # Reset net input
+            self.net_hk[:, t] = 0  # Reset output
+    
+            # Integrate feedback 
+            self.net_h[:, t] += np.dot(self.Wfb.T, cl_output)
+    
+            # Integrate recurrent input from previous state
+            self.net_h[:, t] += np.dot(self.Wr, self.act_h[:, prevt])
+    
+            # Apply activation function
+            self.act_h[:, t] += np.tanh(0.9 * self.act_h[:, prevt] + self.net_h[:, t])
+            self.act_h[:, t] = np.clip(self.act_h[:, t], -0.8, 0.8)
+    
+            # Compute accumulate output activation
+            self.net_hk[:, t] += np.dot(Wout_new.T, self.act_h[:, t])
+            self.act_hk[:, t] = self.net_hk[:, t]
+    
+            cl_output = self.act_hk[:, t] # Update feedback
+            prevt = t # Update previous timestep
+    
+        # Calculate Normalized RMSE
+        NRMSE = np.sqrt(np.mean((self.act_hk[:, trainingSteps:] - x[:, trainingSteps:]) ** 2) / np.var(x[:, trainingSteps:]))
+    
+        ### PLOT Learned Signal Echo ###
+        x_values = np.arange(trainingSteps, trainingSteps + testingSteps)
+        plt.plot(x_values, self.act_hk[0, trainingSteps:])
+        plt.vlines(trainingSteps, -1.2, 1.2, colors='red')
         
         return self.act_hk, NRMSE
- 
